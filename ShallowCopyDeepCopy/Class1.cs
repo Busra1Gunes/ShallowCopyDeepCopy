8e9c54c8ac65be48040e727b270c5c13d4e1e5ec
Busra1Gunes <busragns1999@hotmail.com>
2024-07-17 14:24:31 +0300

    Add project files.

---
+++ ShallowCopyDeepCopy/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShallowCopyDeepCopy
+{
+    public class Class1
+    {
+        public int İd { get; set; }
+        public string Ad { get; set; }
+
+        public Class1 Clone()
+        {
+            //MemberWiseClone bir sınıfın içerisinde o sınıftan üretilmiş olan o anki nesneyi clonlamamızı sağlayan bir fonksiyondur
+            return (Class1)this.MemberwiseClone();
+
+        }
+    }
+}
---
+++ ShallowCopyDeepCopy/Program.cs
@@ -0,0 +1,27 @@
+﻿using ShallowCopyDeepCopy;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Shallow Copy
+        // Var olan bir nesnenin, değerin, referansının kopyalanmasıdır,
+        //Shallow copy neticesinde eldeki değer çoğaltılamaz, Sadece birden fazla referansla işaretlenmiş olur
+        //Deep Copy
+        //Var olan bir nesne ,deep copy ile kopyalanıyorsa eğer ilgili nesne miktarı çoğalır 
+        //Aynı özelliklere ve değerlere sahip olan belekte farklı bir nesne daha oluşur
+        // Referans turlu değişkenlerin değerlerin default davranışı shallow copydir
+        //Değer türlü değişkenlerde default davranış deep copydir
+
+
+        Class1 m1 = new Class1();
+        m1.Ad = "Büşra";
+        Class1 m2 = m1;
+        m2.Ad = "Kübra";
+        Class1 m3 = m1.Clone();
+
+        Console.WriteLine(m1.Ad);
+        Console.WriteLine(m2.Ad);
+        Console.WriteLine(m3.Ad);
+    }
+}
